7538ff90c3afd2df695de90677fe3f9096c79a19
Kevin Mao <km3290@columbia.edu>
2018-01-20 18:58:50 -0500

    working on integration

--- app.py
+++ app.py
@@ -26,18 +26,25 @@ def receive_message():
        output = request.get_json()
        for event in output['entry']:
           messaging = event['messaging']
-          input_text = message['message']['text']
           for message in messaging:
             if message.get('message'):
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message(input_text)
-                    send_message(recipient_id, response_sent_nontext)
+         		if 'text' in messaging_event['message']:
+						input_text = messaging_event['message']['text']
+						response_sent_text = get_message(input_text)
+						send_message(recipient_id, response_sent_text)
+				else:
+					input_text = 'no text'
+					response_sent_text = get_message(input_text)
+					send_message(recipient_id, response_sent_text)
+                # if message['message'].get('text'):
+                #     response_sent_text = get_message()
+                #     send_message(recipient_id, response_sent_text)
+                # #if user sends us a GIF, photo,video, or any other non-text item
+                # if message['message'].get('attachments'):
+                #     response_sent_nontext = get_message(input_text)
+                #     send_message(recipient_id, response_sent_nontext)
  
     return "Message Processed"
 
@@ -52,7 +59,7 @@ def verify_fb_token(token_sent):
 
 #chooses a random message to send to the user
 def get_message(input_text):
-    if input_text == 'memes':
+    if wit_response(input_text) == 'memes':
     	return 'Here is a dank meme'
     else:
     	sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
