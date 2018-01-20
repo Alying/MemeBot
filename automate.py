9382340a1b1561ac7ec24b73591ae5f8fd1acc68
Chris Yin <chyin@ctemc.org>
2018-01-20 16:13:27 -0500

    timed automation function

--- automate.py
+++ automate.py
@@ -1,1 +1,20 @@
-import datetime
+from time import sleep
+from datetime import datetime, timedelta
+from threading import Timer
+
+def sendMessage(message):
+	print(message)
+
+def scheduleMessage(daysOfWeek, message):
+	now = datetime.now()
+	runat = now + timedelta(seconds = 15)
+	delay = (runat - now).total_seconds()
+
+	print runat
+	print delay
+
+	Timer(delay, sendMessage, [message]).start()
+
+scheduleMessage(3, 'hi')
+sleep(3);
+scheduleMessage(3, 'hey')
