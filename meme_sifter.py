4c12efe997405c3866462d08ec684a6c5e011a43
helenli2727 <helenli2727@gmail.com>
2018-01-20 16:36:35 -0500

    can get link, total likes, and preview picture

--- meme_sifter.py
+++ meme_sifter.py
@@ -8,12 +8,11 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 
 # build the URL for the API endpoint
 host = "https://graph.facebook.com"
-path = "/331078540726047/posts"
+path = "/331078540726047"
+f = "fields=posts.limit(5){likes.summary(true),link,picture}"
 params = urllib.urlencode({"access_token": ACCESS_TOKEN})
 
-#EAAQENu0nml0BAAk5KBOpQZCZC7GzcSvrxlpuB2SSbjGxvSi6dmqVzcWvHtNGZBy6bZCXNEaE7wCmKT0elUSYc9Qgv9pGBgu8uugc0DhTKCBaQmwMZAODktiZCNkCsAFmoDuaSxiQoJ02VjjcApJsoTyaZCC2UWZAUjpdHnCbQYJOQXp9MLHXvX9hY7wyKkZAWI6o6dpnCjkLBRAZDZD
-
-url = "{host}{path}?{params}".format(host=host, path=path, params=params)
+url = "{host}{path}?{f}&{params}".format(host=host, path=path, f=f, params=params)
 
 # open the URL and read the response
 resp = urllib.urlopen(url).read()
