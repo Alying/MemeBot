83e83049c23a9ae2fdb7f3b45c22ef84b39e5d70
Kevin Mao <km3290@columbia.edu>
2018-01-20 20:04:49 -0500

    please work

--- app.py
+++ app.py
@@ -11,57 +11,46 @@ ACCESS_TOKEN = 'EAAQENu0nml0BAA5VZATAIav1GYZBqhQaUwP2gAbybmc4L1mz65fZBZBjzXfx6iH
 VERIFY_TOKEN = 'columbia'
 bot = Bot(ACCESS_TOKEN)
 
-#We will receive messages that Facebook sends our bot at this endpoint 
-@app.route("/", methods=['GET', 'POST']) #receive, send 
-def receive_message():
-    if request.method == 'GET':
-        """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook.""" 
-        token_sent = request.args.get("hub.verify_token")
-        return verify_fb_token(token_sent)
-    #if the request was not get, it must be POST and we can just proceed with sending a message back to user\
+@app.route('/', methods=['GET'])
+def verify():
+	# Webhook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
 
-@app.route("/", methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def webhook():
-	output = request.get_json()
-	log(output)
-    # get whatever message a user sent the bot
-    if output['object'] == 'page':
-		for event in output['entry']:
-         	messaging = event['messaging']
-          	for message in messaging:
-            	if message.get('message'):
-                	#Facebook Messenger ID for user so we know where to send response back to
-	                recipient_id = message['sender']['id']
-	                sender_id = message['sender']['id']
-	         		if 'text' in message['message']:
-						input_text = message['message']['text']
-						response_sent_text = get_message(input_text)
-						send_message(recipient_id, response_sent_text)
+	data = request.get_json()
+	log(data)
+
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
+
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
 					else:
-						input_text = 'no text'
-						response_sent_text = get_message(input_text)
-						send_message(recipient_id, response_sent_text)
-	                # if message['message'].get('text'):
-	                #     response_sent_text = get_message()
-	                #     send_message(recipient_id, response_sent_text)
-	                # #if user sends us a GIF, photo,video, or any other non-text item
-	                # if message['message'].get('attachments'):
-	                #     response_sent_nontext = get_message(input_text)
-	                #     send_message(recipient_id, response_sent_nontext)
- 
-    return "Message Processed"
+						messaging_text = 'no text'
+
+					response = get_message(messaging_text)
+					bot.send_text_message(sender_id, response)
+
+	return "ok", 200
 
 
-def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
-    if token_sent == VERIFY_TOKEN:
-        return request.args.get("hub.challenge")
-    return 'Invalid verification token'
+def log(message):
+	print(message)
+	sys.stdout.flush()
 
-
-#chooses a random message to send to the user
 def get_message(input_text):
     value = wit_response(input_text)
     if value == 'memes':
@@ -71,15 +60,6 @@ def get_message(input_text):
     # return selected item to the user
     	return random.choice(sample_responses)
 
-#uses PyMessenger to send response to user
-def send_message(recipient_id, response):
-    #sends user the text message provided via input response parameter
-    bot.send_text_message(recipient_id, response)
-    return "success"
-
-def log(message):
-	print(message)
-	sys.stdout.flush()
-
 if __name__ == "__main__":
-    app.run(debug = True)
+	app.run(debug = True, port = 80)
+
